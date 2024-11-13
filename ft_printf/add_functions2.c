1486971f7aef1a1695344b856859dddf56637d7d
houssam <142263949+houssam142@users.noreply.github.com>
2024-11-13 17:12:14 +0100

    Add files via upload

---
+++ ft_printf/add_functions2.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_functions2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hounejja <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 18:06:48 by hounejja          #+#    #+#             */
+/*   Updated: 2024/11/12 18:07:42 by hounejja         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+int	dectohexl(unsigned int n)
+{
+	int		count;
+	char	*res;
+
+	res = "0123456789abcdef";
+	count = 0;
+	if (n >= 16)
+	{
+		count += dectohexl(n / 16);
+	}
+	ft_putchar(res[n % 16]);
+	count++;
+	return (count);
+}
+
+int	putp(void *ptr)
+{
+	unsigned long long	a;
+	int					count;
+
+	count = 0;
+	a = (unsigned long long)ptr;
+	dectohexl(a);
+	count++;
+	return (count);
+}
---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hounejja <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 15:23:09 by hounejja          #+#    #+#             */
+/*   Updated: 2024/11/11 15:34:26 by hounejja         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+int	checkformats(va_list p, const char *s2)
+{
+	int	count;
+
+	count = 0;
+	if (*s2 == 'c')
+		count += ft_putchar(va_arg(p, int));
+	else if (*s2 == 's')
+		count += ft_putstr(va_arg(p, char *));
+	else if (*s2 == 'd' || *s2 == 'i')
+		count += ft_putnbr(va_arg(p, int));
+	else if (*s2 == 'u')
+		count += ft_putnbr_pos(va_arg(p, unsigned int));
+	else if (*s2 == 'x')
+		count += dectohexl(va_arg(p, unsigned int));
+	else if (*s2 == 'X')
+		count += dectohexu(va_arg(p, unsigned int));
+	else if (*s2 == 'p')
+		count += putp(va_arg(p, void *));
+	else if (*s2 == '%')
+		ft_putchar('%');
+	return (count);
+}
+
+int	ft_printf(const char *w, ...)
+{
+	int		count;
+	size_t	i;
+	va_list	lo;
+
+	if (!w)
+		return (write(1, "-1", 2));
+	count = 0;
+	i = 0;
+	va_start(lo, w);
+	while (w[i])
+	{
+		if (w[i] == '%')
+		{
+			i++;
+			count += checkformats(lo, &w[i]);
+		}
+		i++;
+	}
+	va_end(lo);
+	return (count);
+}
+//int	main(void)
+//{
+//	//ft_printf("%p %d %s %x", "dasjh ", 423, " daiuw", 244);
+//	ft_printf("%X %p %x %d", 234, 234, 234);
+//}
