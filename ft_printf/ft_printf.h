b934dca40eff6b2db6adaf3cf1596c4a638e0a58
houssam <142263949+houssam142@users.noreply.github.com>
2024-11-26 14:38:01 +0100

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hounejja <hounejja@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 20:59:18 by hounejja          #+#    #+#             */
+/*   Updated: 2024/11/26 11:04:21 by hounejja         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_printf(const char *w, ...);
+int		ft_putchar(char c);
+int		ft_putnbr(int nb);
+int		ft_putstr(const char *str);
+int		ft_putnbr_pos(unsigned int n);
+int		dectohexlu(unsigned int a, const char *s1);
+int		dectohexl(unsigned long long n);
+int		putp(void *ptr);
+int		ft_strchr(const char *str, int search_str);
+
+#endif
