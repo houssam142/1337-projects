ddf05f4ac43e8cf13737d10864ff74536a5eb0b6
houssam <142263949+houssam142@users.noreply.github.com>
2025-02-04 16:12:28 +0100

    Add files via upload

---
+++ push_swap/srcs/reverse_rotate.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hounejja <hounejja@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 08:06:40 by hounejja          #+#    #+#             */
+/*   Updated: 2025/02/03 09:06:49 by hounejja         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/push_swap.h"
+
+void	rev_rotate(t_stack **stack)
+{
+	t_stack	*tmp;
+	t_stack	*tail;
+	t_stack	*before_tail;
+
+	tail = get_stack_bottom(*stack);
+	before_tail = get_stack_before_bottom(*stack);
+	tmp = *stack;
+	*stack = tail;
+	(*stack)->next = tmp;
+	before_tail->next = NULL;
+}
+
+void	do_rra(t_stack **stack_a)
+{
+	rev_rotate(stack_a);
+	ft_putstr("rra\n");
+}
+
+void	do_rrb(t_stack **stack_b)
+{
+	rev_rotate(stack_b);
+	ft_putstr("rrb\n");
+}
+
+void	do_rrr(t_stack **stack_a, t_stack **stack_b)
+{
+	rev_rotate(stack_a);
+	rev_rotate(stack_b);
+	ft_putstr("rrr\n");
+}
