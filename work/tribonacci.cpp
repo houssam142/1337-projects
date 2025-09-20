0bdbfb2d4915ea7034b9f1e19e00e804ff37a4c7
Houssam Ounejjar <hounejja@c1r9p3.1337.ma>
2025-09-20 11:02:22 +0100

    hellofs

---
+++ work/tribonacci.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tribonacci.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hounejja <hounejja@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/20 10:24:22 by hounejja          #+#    #+#             */
+/*   Updated: 2025/09/20 10:34:16 by hounejja         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void    tribonacci(vector<int> &arr, int length)
+{
+    int size = arr.size();
+    if (size == length)
+        return ;
+    else if (size > length)
+    {
+        arr.pop_back();
+        size--;
+        tribonacci(arr, length);
+    }
+    if (size != length)
+    {
+        int sum = arr[size - 3] + arr[size - 2] + arr[size - 1];
+        arr.push_back(sum);
+        tribonacci(arr, length);
+    }
+}
+
+int main()
+{
+    vector<int> arr = {10, 20, 30};
+    tribonacci(arr, 2);
+    cout << "[";
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i];
+        if (i != arr.size() - 1)
+            cout << ", ";
+    }   
+    cout << "]" << "\n";
+    return 0;
+}
--- work/tribonacci.js
+++ work/tribonacci.js
@@ -1,21 +1,25 @@
 function tribonacciSequence(startSequence, length)
 {
     let size = startSequence.length
-    console.log(startSeqence[size - 3]);
     if (size == length)
-      return startSequence;
-    let num = 0
-    if (size <= length)
+      return startSeqence;
+    else if (size > length)
     {
-        tribonacciSequence(startSequence[size - 3] + startSeqence[size - 2] + startSeqence[size - 1], length)
-        size++;
+      startSeqence.length = length;
+      return startSeqence;
+    }
+    if (size != length)
+    {  
+      let sum = startSequence[size - 3] + startSeqence[size - 2] + startSeqence[size - 1];
+      startSeqence.push(sum);
+      tribonacciSequence(startSeqence, length)
     }
     return startSeqence;
 }
 
 let startSeqence = [0, 0, 1];
 
-let length = 20
+let length = 0
 
 let sequence = tribonacciSequence(startSeqence, length)
 
