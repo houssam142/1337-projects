4353f21b55c119d247bcb10e7083ab2257973b46
Houssam Ounejjar <hounejja@c1r9p10.1337.ma>
2025-10-10 20:39:24 +0100

    hello wa

---
+++ cpp_modules/cpp_01/ex00/Zombie.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hounejja <hounejja@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/10 20:05:48 by hounejja          #+#    #+#             */
+/*   Updated: 2025/10/10 20:07:43 by hounejja         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+	private:
+		std::string name;
+		void	announce(void);
+};
+
+
+#endif
