23f6a7324438dd151a74c23f4c8256cc4b4edd0c
houssam142 <houssamounejjar@gmail.com>
2025-12-13 08:14:59 +0100

    another commit 10/12

--- cpp_modules/cpp_05/ex02/main.cpp
+++ cpp_modules/cpp_05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hounejja <hounejja@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 20:29:21 by hounejja          #+#    #+#             */
-/*   Updated: 2025/12/12 20:55:02 by hounejja         ###   ########.fr       */
+/*   Updated: 2025/12/13 08:14:10 by hounejja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,42 @@
 
 int main()
 {
-    Bureaucrat low("Low Grade", 150);
-    Bureaucrat mid("Mid grade", 75);
-    Bureaucrat high("High grade", 1);
-    ShrubberyCreationForm garden("garden");
-    RobotomyRequestForm bender("Bender");
-    PresidentialPardonForm zaphod("Zaphod");
+    try
+    {
+        Bureaucrat low("Low Grade", 150);
+        Bureaucrat mid("Mid grade", 75);
+        Bureaucrat high("High grade", 1);
+        ShrubberyCreationForm garden("garden");
+        RobotomyRequestForm bender("Bender");
+        PresidentialPardonForm zaphod("Zaphod");
 
-    std::cout << low << '\n';
-    std::cout << mid << '\n';
-    std::cout << high << '\n';
+        std::cout << low << '\n';
+        std::cout << mid << '\n';
+        std::cout << high << '\n';
 
-    std::cout << garden << '\n';
-    std::cout << bender << '\n';
-    std::cout << zaphod << '\n';
+        std::cout << garden << '\n';
+        std::cout << bender << '\n';
+        std::cout << zaphod << '\n';
 
-    low.signForm(garden);
-    low.signForm(bender);
-    low.signForm(zaphod);
+        low.signForm(garden);
+        low.signForm(bender);
+        low.signForm(zaphod);
 
-    mid.signForm(garden);
-    mid.signForm(bender);
-    mid.signForm(zaphod);
+        mid.signForm(garden);
+        mid.signForm(bender);
+        mid.signForm(zaphod);
 
-    high.signForm(garden);
-    high.signForm(bender);
-    high.signForm(zaphod);
+        high.signForm(garden);
+        high.signForm(bender);
+        high.signForm(zaphod);
 
-    high.executeForm(garden);
-    mid.executeForm(bender);
-    low.executeForm(zaphod);
+        high.executeForm(garden);
+        mid.executeForm(bender);
+        low.executeForm(zaphod);
+    }
+    catch (std::exception & e)
+    {
+        std::cerr << e.what();
+    }
     return 0;
 }
