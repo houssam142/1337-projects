aeffb3ca671a2fd31b404265faad25ca0731b1e1
houssam142 <houssamounejar@gmail.com>
2025-12-19 01:30:46 +0100

    another commit 12/10

--- cpp_modules/cpp_06/ex01/main.cpp
+++ cpp_modules/cpp_06/ex01/main.cpp
@@ -5,5 +5,6 @@ int main()
   Data* ptr = new Data();
   uintptr_t w =  Serializer::serialize(ptr);
   std::cout << w << '\n';
+  delete ptr;
   return 0;
 }
--- cpp_modules/cpp_06/ex02/main.cpp
+++ cpp_modules/cpp_06/ex02/main.cpp
@@ -66,5 +66,6 @@ int main()
 	Base* ptr = generate();
 	identify(ptr);
 	identify(*ptr);
+	delete ptr;
 	return 0;
 }
