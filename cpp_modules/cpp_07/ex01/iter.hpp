0f2fa3f0695a22c5aacbc97f51cfa2b0ad4a372a
houssam142 <houssamounejar@gmail.com>
2025-12-21 10:57:53 +0100

    another commit 12/10

--- cpp_modules/cpp_07/ex01/iter.hpp
+++ cpp_modules/cpp_07/ex01/iter.hpp
@@ -2,6 +2,10 @@
 
 #include <iostream>
 
-template <typename T, typename F>
-void iter(T* array, size_t len, F func)
-{}
+template <typename T, typename F> void iter(T* array, int len, F func)
+{
+  for (int i = 0; i < len; i++)
+  {
+    func(array[i]);
+  }
+}
--- cpp_modules/cpp_07/ex01/main.cpp
+++ cpp_modules/cpp_07/ex01/main.cpp
@@ -1,6 +1,23 @@
 #include "iter.hpp"
 
-int main()
-{
-	return 0;
+void print(int x) { std::cout << x << " "; }
+void increment(int& x) { x++; }
+
+template<typename T>
+struct Printer {
+    void operator()(T x) { std::cout << x << " "; }
+};
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    
+    std::cout << "Before: ";
+    iter(arr, 5, print); std::cout << std::endl;
+    
+    iter(arr, 5, increment);
+    
+    std::cout << "After: ";
+    iter(arr, 5, print); std::cout << std::endl;
+    
+    return 0;
 }
