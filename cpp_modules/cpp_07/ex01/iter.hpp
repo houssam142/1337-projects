4311bc0131cf1ce1b71892c8bd4577452a67e4e7
houssam142 <houssamounejjar@gmail.com>
2025-12-21 13:11:59 +0100

    another commit 16/12

--- cpp_modules/cpp_07/ex01/iter.hpp
+++ cpp_modules/cpp_07/ex01/iter.hpp
@@ -2,10 +2,16 @@
 
 #include <iostream>
 
-template <typename T, typename F> void iter(T* array, int len, F func)
+template <typename T, typename F>
+void iter(T* array, int len, F func)
 {
   for (int i = 0; i < len; i++)
-  {
     func(array[i]);
-  }
+}
+
+template <typename T, typename F>
+void iter(const T* array, int len, F func)
+{
+	for (int i = 0; i < len; i++)
+		func(array[i]);
 }
--- cpp_modules/cpp_07/ex01/main.cpp
+++ cpp_modules/cpp_07/ex01/main.cpp
@@ -1,23 +1,12 @@
 #include "iter.hpp"
 
 void print(int x) { std::cout << x << " "; }
-void increment(int& x) { x++; }
-
-template<typename T>
-struct Printer {
-    void operator()(T x) { std::cout << x << " "; }
-};
 
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
+    int arr[] = {1, 2, 3};
+    const int arr1[] = {4, 5, 6};
     
-    std::cout << "Before: ";
-    iter(arr, 5, print); std::cout << std::endl;
-    
-    iter(arr, 5, increment);
-    
-    std::cout << "After: ";
-    iter(arr, 5, print); std::cout << std::endl;
-    
+    iter(arr, 3, print); std::cout << std::endl;
+    iter(arr1, 3, print); std::cout << std::endl;
     return 0;
 }
