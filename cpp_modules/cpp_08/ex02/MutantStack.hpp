f2251508077a1828e0f5ab2dfd9845107876df43
houssam142 <houssamounejar@gmail.com>
2025-12-23 23:02:58 +0100

    another commit 12/10

--- cpp_modules/cpp_08/ex02/MutantStack.hpp
+++ cpp_modules/cpp_08/ex02/MutantStack.hpp
@@ -24,4 +24,7 @@ class MutantStack: public std::stack<T>
 
 		iterator begin() {return this->c.begin();}
 		iterator end() {return this->c.end();}
+
+		const_iterator begin() const {return this->c.begin();}
+		const_iterator end() const {return this->c.end();}
 };
