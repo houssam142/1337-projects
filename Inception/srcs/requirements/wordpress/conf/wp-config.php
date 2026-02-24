5090076a3866f9b4eccd2f5843c7689f60c3870f
houssam <houssam@hounejja.hounejja.42.fr>
2026-02-23 17:54:47 -0700

    dawhkawf

---
+++ Inception/srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('WP_HOME', 'https://localhost');
+define('WP_SITEURL', 'https://localhost');
+
+define('DB_NAME', 'houssam');
+define('DB_USER', 'hounejja');
+define('DB_PASSWORD', 'Hello1234$');
+define('DB_HOST', 'mariadb');
+
