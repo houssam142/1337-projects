83f72dfaf7851699e0d74db1cf0f0aae2553a274
houssam <houssam@hounejja.hounejja.42.fr>
2026-02-26 02:35:25 -0700

    dawkdawwf

---
+++ Inception/srcs/requirements/mariadb/tools/create_db.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p /run/mysqld
+
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+
+mysqld --user=mysql &
+sleep 5
+
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_DATABASE}.* TO '${DB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+exec mysqld
