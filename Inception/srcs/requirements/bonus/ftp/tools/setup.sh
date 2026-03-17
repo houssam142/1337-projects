dc9df4ed02def17edec490758a144670a057c7ae
houssam <hounejja@1337.student.ma>
2026-03-17 14:16:58 -0400

    building static web

--- Inception/srcs/requirements/bonus/ftp/tools/setup.sh
+++ Inception/srcs/requirements/bonus/ftp/tools/setup.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+set -e
+
+useradd -m -s /bin/bash $FTP_USER
+echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+
+mkdir -p /var/run/vsftpd/empty
+
+usermod -d /var/www/html "$FTP_USER"
+
+chown -R $FTP_USER:$FTP_USER /var/www/html
+
+exec /usr/sbin/vsftpd /etc/vsftpd.conf
